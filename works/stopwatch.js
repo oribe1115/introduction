b88c10a8a9a07e715a2665d716107dd2d9898387
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-05-09 20:01:08 +0900

    スタートボタンでスタート＆ストップができるようになった

--- works/stopwatch.js
+++ works/stopwatch.js
@@ -1,11 +1,21 @@
 let time = 0
-let keepOn = true
-const countUp = () => {
-    keepOn = true
-    while(keepOn){
-        time++
-        setInterval(1000)
+let keepOn = 0
+const startCountUp = () => {
+    if(keepOn == 0){
+        keepOn = 1
+        timeID = setInterval("countUp()", 1000)
+    } else{
+        keepOn = 0
+        clearInterval(timeID)
     }
+    
+}
+const countUp = () => {
+    
+    const timeElement = document.querySelector('#time')
+    time++
+    timeElement.innerText = '時間：' + time
+
 }
 
 const stop = () => {
