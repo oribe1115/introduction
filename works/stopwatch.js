b403ab5d6409f6a51cdc6a7d523609cc6c357f53
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-05-10 09:01:09 +0900

    スタート＆ストップの表示が切り替わるようになった。

--- works/stopwatch.js
+++ works/stopwatch.js
@@ -1,23 +1,22 @@
 let time = 0
 let keepOn = 0
 const startCountUp = () => {
+    const startButtonElement = document.querySelector("#startButton")
     if(keepOn == 0){
+        startButtonElement.innerText = "STOP"
         keepOn = 1
         timeID = setInterval("countUp()", 1000)
     } else{
+        startButtonElement.innerText = "START"
         keepOn = 0
         clearInterval(timeID)
     }
     
 }
 const countUp = () => {
-    
     const timeElement = document.querySelector('#time')
     time++
     timeElement.innerText = '時間：' + time
 
 }
 
-const stop = () => {
-    keepOn = false
-}
